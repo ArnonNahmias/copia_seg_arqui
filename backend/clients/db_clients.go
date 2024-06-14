0c8a0e7d758120695e880cbec6abecde914e9bc3
felipeganame <2111762@ucc.edu.ar>
2024-06-14 18:04:27 -0300

    hola

--- backend/clients/db_clients.go
+++ backend/clients/db_clients.go
@@ -11,11 +11,12 @@ import (
 	"gorm.io/gorm"
 )
 
+
 var DB *gorm.DB
 
 func InitDB() {
 	log.Println("Initializing database...")
-	dsn := "root:admin@tcp(127.0.0.1:3306)/proyecto?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:pass@tcp(127.0.0.1:3306)/proyecto?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -26,6 +27,7 @@ func InitDB() {
 	Migrate()
 	SeedDB()
 }
+
 
 func Migrate() {
 	log.Println("Migrating database...")
@@ -50,7 +52,18 @@ func SeedDB() {
 
 	DB.FirstOrCreate(&admin, dao.Usuario{NombreUsuario: "admin"})
 	DB.FirstOrCreate(&user, dao.Usuario{NombreUsuario: "user"})
+
+	log.Println("Seeding database...")
+	cursos := []dao.Course{
+		{Nombre: "Ingles B2", Dificultad: "Medio", Precio: 45, Direccion: "José Roque Funes 1511 X5000ABE Córdoba", ImageURL: "https://blog.hubspot.com/hs-fs/hubfs/freeonlinecourses-1.webp?width=595&height=400&name=freeonlinecourses-1.webp"},
+	}
+	for _, curso := range cursos {
+		DB.Create(&curso)
+	}
+	log.Println("Database seeded successfully")
 }
+
+
 
 func GetCourses1() ([]dao.Course, error) {
 	var courses []dao.Course
